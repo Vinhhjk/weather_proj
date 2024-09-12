5c0e297d7f5c4337d92c205a9fe5e4f3b945eb0e
Vinhhjk <nguyenvinh5005@gmail.com>
2024-09-12 10:01:20 +0700

    message

--- env/api_call.py
+++ env/api_call.py
@@ -4,13 +4,11 @@ from dotenv import load_dotenv
 import json
 load_dotenv()
 city = 'Ho chi minh'
-rsp = requests.get(f'http://api.weatherapi.com/v1/history.json?key={os.getenv("API_KEY")}&q={city}&dt=2024-09-12').json()
+rsp = requests.get(f'http://api.weatherapi.com/v1/history.json?key={os.getenv("API_KEY")}&q={city}&dt=2024-09-11').json()
 
 with open("weather.json", "w") as f:
     json.dump(rsp, f, indent=4)
 name = rsp['location']['name']
 country = rsp['location']['country']
 localtime = rsp['location']['localtime']
-temp_c = rsp['current']['temp_c']
-wind_speed = rsp['current']['wind_kph']
-print(f'City: {name}, Country: {country}, Local Time: {localtime}, Temperature: {temp_c}C, Wind Speed: {wind_speed}kph')
+print(f'City: {name}, Country: {country}, Local Time: {localtime}')
