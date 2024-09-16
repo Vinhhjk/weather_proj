bbeff18daaf2c40bbc7cd8896c6050aa97948511
Vinhhjk <nguyenvinh5005@gmail.com>
2024-09-16 08:30:30 +0700

    edit attribute

--- env/convert_to_csv.py
+++ env/convert_to_csv.py
@@ -2,10 +2,12 @@ import glob
 import json
 import csv
 
-data_csv = [['date','maxtemp_c','mintemp_c','windspeed_kph','avghumidity']]
+fieldname = ['province','date','maxtemp_c','mintemp_c','windspeed_kph','avghumidity']
+data_csv = []
 files = glob.glob('weather_*.json')
 for file in files:
     data = json.load(open(file))
+    province = data['location']['name']
     detailed_data= data['forecast']['forecastday']
     for i in detailed_data:
        date = i['date']
@@ -13,10 +15,11 @@ for file in files:
        min_c = i['day']['mintemp_c']
        maxwind_kph = i['day']['maxwind_kph']
        avghumidity = i['day']['avghumidity']
-       data_to_import = [date,max_c,min_c,maxwind_kph,avghumidity]
+       data_to_import = {'province': province, 'date':date,'maxtemp_c':max_c,'mintemp_c':min_c,'windspeed_kph':maxwind_kph,'avghumidity':avghumidity}
        data_csv.append(data_to_import)
 
 with open('weather_data.csv', 'a', newline='') as csvfile:
-    writer = csv.writer(csvfile)
+    writer = csv.DictWriter(csvfile, fieldnames = fieldname)
+    writer.writeheader()
     writer.writerows(data_csv)
 
